08731b6a514dff1f47d70069d3f64865bc560321
alies_a <arnaud.alies@epitech.eu>
2016-05-23 18:20:08 +0200

    ok

--- potato.py
+++ potato.py
@@ -3,10 +3,10 @@
 import potato.load
 
 def main():
-	#item = potato.load.item("Chroma 2 Case Key", 730)
+	#item = potato.load.item_json("Chroma 2 Case Key", 730)
 	#print(item)
 	
-	item_list = potato.load.item_list(0, 3)
+	item_list = potato.load.item_list_json(0, 3)
 	print(item_list["results_html"])
 
 if (__name__ == "__main__"):
--- potato/load.py
+++ potato/load.py
@@ -12,20 +12,22 @@ def request_json(link, values):
 	json_file = json.loads(page.decode('utf-8'))
 	return (json_file)
 
-def item(name, appid, country=COUNTRY, currency=CURRENTY):
+def item_json(name, appid, country=COUNTRY, currency=CURRENTY):
 	values = {
 	"country" : country,
 	"currenty" : str(currency),
 	"appid" : str(appid),
-	"market_hash_name" : name
+	"market_hash_name" : name,
+	"format" : "json"
 	}
 	json_file = request_json(ITEM_URL, values)
 	return (json_file)
 
-def item_list(start, count):
+def item_list_json(start, count):
 	values = {
 	"start" : str(start),
-	"count" : str(count)
+	"count" : str(count),
+	"format" : "json"
 	}
 	json_file = request_json(LIST_URL, values)
 	return (json_file)

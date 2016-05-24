0f1a2bce188838f4b20dfadad6dabbde2511aa14
alies_a <arnaud.alies@epitech.eu>
2016-05-24 16:28:12 +0200

    ok

--- potato.py
+++ potato.py
@@ -5,24 +5,43 @@ import potato.parse
 import urllib.error
 import time
 
+def get_diff(item_data):
+	diff = 0.0
+	try:
+		low = item_data["lowest_price"].replace("€", "").replace(",", ".")
+		med = item_data["median_price"].replace("€", "").replace(",", ".")
+		flow = float(low)
+		fmed = float(med)
+		diff = fmed - flow
+		#print(diff)
+	except:
+		pass
+	return (diff)
+		
+
 def test():
+	max_diff = 0.0
+	max_name = ""
 	x = 0
-	while (x < 10):
-		item_list_json = potato.load.item_list(x, 100)
+	while (x < 1000):
+		item_list_json = potato.load.item_list(x + 400, 100)
 		item_list = potato.parse.item_list(item_list_json)
 		i = 0
 		while (i < len(item_list)):
 			item = item_list[i]
 			try:
 				item_data = potato.load.item(item[0], int(item[1]))
+				if (get_diff(item_data) > max_diff):
+					max_name = item[0]
+					max_diff = get_diff(item_data)
+					print("NEW MAX DIFF = " + str(max_diff))
 				print(item)
 				print(item_data)
-				time.sleep(2)
 				i += 1
 			except (urllib.error.HTTPError):
 				time.sleep(60)
 				item_data = potato.load.item(item[0], int(item[1]))
-		x += 1
+		x += 100
 
 def main():
 	test()

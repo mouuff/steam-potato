8bf46bcf45e0f4e88113fc0616df3b2ac57191e7
alies_a <arnaud.alies@epitech.eu>
2016-05-24 15:22:37 +0200

    added test function

--- potato.py
+++ potato.py
@@ -2,8 +2,31 @@
 
 import potato.load
 import potato.parse
+import urllib.error
+import time
+
+def test():
+	x = 0
+	while (x < 10):
+		item_list_json = potato.load.item_list(x, 100)
+		item_list = potato.parse.item_list(item_list_json)
+		i = 0
+		while (i < len(item_list)):
+			item = item_list[i]
+			try:
+				item_data = potato.load.item(item[0], int(item[1]))
+				print(item)
+				print(item_data)
+				time.sleep(2)
+				i += 1
+			except (urllib.error.HTTPError):
+				time.sleep(60)
+				item_data = potato.load.item(item[0], int(item[1]))
+		x += 1
 
 def main():
+	test()
+	'''
 	item = potato.load.item("MLG Columbus 2016 Challengers (Holo-Foil)", 730)
 	print(item)
 	
@@ -11,6 +34,7 @@ def main():
 	item_list = potato.parse.item_list(item_list_json)
 	for item in item_list:
 		print(item)
+	'''
 
 if (__name__ == "__main__"):
 	main()

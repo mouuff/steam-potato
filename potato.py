4010682bf994821b9e81fabf9ab9b238273c2032
alies_a <arnaud.alies@epitech.eu>
2016-05-24 14:47:46 +0200

    ok

--- potato.py
+++ potato.py
@@ -4,11 +4,13 @@ import potato.load
 import potato.parse
 
 def main():
-	#item = potato.load.item("Chroma 2 Case Key", 730)
-	#print(item)
+	item = potato.load.item("MLG Columbus 2016 Challengers (Holo-Foil)", 730)
+	print(item)
 	
-	item_list = potato.load.item_list(0, 3)
-	potato.parse.item_list(item_list)
+	item_list_json = potato.load.item_list(0, 10)
+	item_list = potato.parse.item_list(item_list_json)
+	for item in item_list:
+		print(item)
 
 if (__name__ == "__main__"):
 	main()
--- potato/parse.py
+++ potato/parse.py
@@ -1,10 +1,23 @@
 
+import urllib.parse
 import re
 from potato.constants import MARKET_URL
 
 def item_list(json_list):
+	'''
+	Converts a json list of items to a usable list
+	Steam API returns the list of items in a json which contains html
+	The role of this function is to parse the html part
+	this function should be called after:
+	potato.parse.item_list(start, count)
+	'''
 	html = json_list["results_html"]
-	expr = r"href=\"{market_url}/(.*?)\" id=\"resultlink_.\">"
+	expr = r"href=\"{market_url}/(.*?)/(.*?)\" id=\"resultlink_.\">"
 	expr = expr.format(market_url=MARKET_URL)
 	regex = re.compile(expr)
-	print(regex.findall(html))
+	item_tulpe = regex.findall(html)
+	item_list = []
+	for item in item_tulpe:
+		item_name = urllib.parse.unquote(item[1])
+		item_list.append([item_name, item[0]])
+	return (item_list)

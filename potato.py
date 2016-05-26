d4c2963ef47fd23b707b7f0ba940e477c37942c9
alies_a <arnaud.alies@epitech.eu>
2016-05-26 14:43:03 +0200

    ok

--- potato.py
+++ potato.py
@@ -8,37 +8,41 @@ import re
 
 STEP = 100
 
+def get_item_price(item, name="sale_price"):
+	price = item[name]
+	price = re.findall(r'\$(.*) USD', price)[0]
+	result = float(price)
+	return (result)
+
 def get_price_diff(item):
-	normal_price = item["normal_price"]
-	sale_price = item["sale_price"]
-	normal_price = re.findall(r'\$(.*) USD', normal_price)[0]
-	sale_price = re.findall(r'\$(.*) USD', sale_price)[0]
-	return (float(normal_price) - float(sale_price))
+	normal_price = get_item_price(item, "normal_price")
+	sale_price = get_item_price(item, "sale_price")
+	return (normal_price - sale_price)
 
 def test():
 	diff_max = 0
 	item_max = None
 	x = 0
-	while (x < STEP * 100):
+	while (x < STEP * 10000):
 		try:
 			item_list = potato.load.item_list(x, STEP)
 			print("Loaded {x} items at {range}".format(x=STEP, range=x))
 			#print(item_list_json["results_html"])
 			#item_list = potato.parse.item_list(item_list_json)
 			for item in item_list:
-				print(item)
-				if (get_price_diff(item) > 1):
+				#print(item)
+				if (get_price_diff(item) > 1 and get_item_price(item) <= 25):
 					diff_max = get_price_diff(item)
 					item_max = item
 					print("NEW DIFF:")
 					print(item)
 					print(diff_max)
 				#print(get_price_diff(item))
-			
 			x += STEP
 		except (urllib.error.HTTPError):
 			print("429 Error")
 			time.sleep(5)
+		time.sleep(2)
 
 def main():
 	test()

47f757727d931648363c1d81a0ccb68c8c662819
alies_a <arnaud.alies@epitech.eu>
2016-09-20 13:59:30 +0200

    ok

--- potato/load.py
+++ potato/load.py
@@ -5,44 +5,44 @@ import json
 import potato.parse
 from potato.constants import COUNTRY, CURRENTY, ITEM_URL, LIST_URL
 
+
 def request_json(link, values):
-	'''
-	Get json file from steam website
-	'''
-	request = urllib.parse.urlencode(values)
-	url = "{url}?{req}".format(url=link, req=request)
-	rep = urllib.request.urlopen(url)
-	page = rep.read()
-	json_file = json.loads(page.decode('utf-8'))
-	return (json_file)
+    '''Get json file from steam website
+    '''
+    request = urllib.parse.urlencode(values)
+    url = "{url}?{req}".format(url=link, req=request)
+    rep = urllib.request.urlopen(url)
+    page = rep.read()
+    json_file = json.loads(page.decode('utf-8'))
+    return (json_file)
+
 
 def item(name, appid, country=COUNTRY, currency=CURRENTY):
-	'''
-	returns basic item information by name and appid
-	appid defines the name of steam game
-	it is defined by APPS constant
-	'''
-	values = {
-	"country" : country,
-	"currency" : str(currency),
-	"appid" : str(appid),
-	"market_hash_name" : name,
-	"format" : "json"
-	}
-	json_file = request_json(ITEM_URL, values)
-	return (json_file)
+    '''Returns basic item information by name and appid
+    appid defines the name of steam game
+    it is defined by APPS constant
+    '''
+    values = {
+    "country": country,
+    "currency": str(currency),
+    "appid": str(appid),
+    "market_hash_name": name,
+    "format": "json"
+    }
+    json_file = request_json(ITEM_URL, values)
+    return (json_file)
+
 
 def item_list(start, count):
-	'''
-	Load item list
-	returns a json which contains list information
-	the actual list is stored in html
-	'''
-	values = {
-	"start" : str(start),
-	"count" : str(count),
-	"format" : "json"
-	}
-	json_file = request_json(LIST_URL, values)
-	result = potato.parse.item_list(json_file)
-	return (result)
+    '''Load item list
+    returns a json which contains list information
+    the actual list is stored in html
+    '''
+    values = {
+    "start": str(start),
+    "count": str(count),
+    "format": "json"
+    }
+    json_file = request_json(LIST_URL, values)
+    result = potato.parse.item_list(json_file)
+    return (result)
--- potato/parse.py
+++ potato/parse.py
@@ -3,57 +3,57 @@ import urllib.parse
 import re
 from potato.constants import MARKET_URL
 
+
 def item_name(html):
-	'''
-	Get item name and appid
-	'''
-	expr = r"href=\"{market_url}/(.*?)/(.*?)\" id=\"resultlink_"
-	expr = expr.format(market_url=MARKET_URL)
-	appid, name = re.findall(expr, html)[0]
-	name = urllib.parse.unquote(name)
-	return (name, appid)
+    '''Get item name and appid
+    '''
+    expr = r"href=\"{market_url}/(.*?)/(.*?)\" id=\"resultlink_"
+    expr = expr.format(market_url=MARKET_URL)
+    appid, name = re.findall(expr, html)[0]
+    name = urllib.parse.unquote(name)
+    return (name, appid)
+
 
 def html_get_span(html, name):
-	'''
-	Get span from name
-	this function is used to get normal price, sale, price
-	'''
-	expr = r'<span class=\"{name}\">(.*?)</span>'.format(name=name)
-	span = re.findall(expr, html)
-	return (span[0])
+    '''Get span from name
+    this function is used to get normal price, sale, price
+    '''
+    expr = r'<span class=\"{name}\">(.*?)</span>'.format(name=name)
+    span = re.findall(expr, html)
+    return (span[0])
+
 
 def html_item(html):
-	'''
-	Get item information from html
-	this function should be called after
-	potato.parse.item_list(json_list) function
-	returns a dictionary of containing item informations:
-	name, appid, normal_price, sale_price, quantity
-	'''
-	name, appid = item_name(html)
-	normal_price = html_get_span(html, "normal_price")
-	sale_price = html_get_span(html, "sale_price")
-	quantity = html_get_span(html, "market_listing_num_listings_qty")
-	info = {
-	"name" : name,
-	"appid" : appid,
-	"normal_price" : normal_price,
-	"sale_price" : sale_price,
-	"quantity" : quantity
-	}
-	return (info)
+    '''Get item information from html
+    this function should be called after
+    potato.parse.item_list(json_list) function
+    returns a dictionary of containing item informations:
+    name, appid, normal_price, sale_price, quantity
+    '''
+    name, appid = item_name(html)
+    normal_price = html_get_span(html, "normal_price")
+    sale_price = html_get_span(html, "sale_price")
+    quantity = html_get_span(html, "market_listing_num_listings_qty")
+    info = {
+    "name": name,
+    "appid": appid,
+    "normal_price": normal_price,
+    "sale_price": sale_price,
+    "quantity": quantity
+    }
+    return (info)
+
 
 def item_list(json_list):
-	'''
-	Converts a json list of items to a usable list
-	Steam API returns the list of items in a json which contains html
-	The role of this function is to parse the html part
-	this function should be called after:
-	potato.parse.item_list(start, count)
-	'''
-	html = json_list["results_html"]
-	html_classes = re.findall(r'<a ((.|\n)*?)</a>', html)
-	result = []
-	for html_class in html_classes:
-		result.append(html_item(html_class[0]))
-	return (result)
+    '''Converts a json list of items to a usable list
+    Steam API returns the list of items in a json which contains html
+    The role of this function is to parse the html part
+    this function should be called after:
+    potato.parse.item_list(start, count)
+    '''
+    html = json_list["results_html"]
+    html_classes = re.findall(r'<a ((.|\n)*?)</a>', html)
+    result = []
+    for html_class in html_classes:
+        result.append(html_item(html_class[0]))
+    return (result)
